8f023ae6a37a49bfe2ca3347937a55da97c15778
Vitalii Borodulin <vitaliiborodulin@yandex.ru>
2021-03-12 17:39:36 +0300

    portfolio done

---
+++ src/js/components/slider.js
@@ -0,0 +1,25 @@
+$('.portfolio__slider').each(function(){
+
+	
+
+	$(this).find('.portfolio__slider-main').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		// lazyLoad: 'ondemand',
+		infinite: false,
+		asNavFor: $(this).find('.portfolio__slider-nav')
+	});
+
+	$(this).find('.portfolio__slider-nav').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		asNavFor: $(this).find('.portfolio__slider-main'),
+		arrows: false,
+		// centerMode: true,
+		infinite: false,
+		focusOnSelect: true,
+		// lazyLoad: 'ondemand'
+	});
+
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,13 @@
 //= ../../node_modules/jquery.maskedinput/src/jquery.maskedinput.js
+//= ../../node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min.js
+//= ../../node_modules/slick-carousel/slick/slick.min.js
 
 $(function(){
 
 	$('input[type="tel"]').mask("+7 (999) 999-9999");
 
 	//= components/map.js
+	//= components/slider.js
 
 });
 
